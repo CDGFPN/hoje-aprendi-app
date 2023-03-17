a1cc92b64eec29553cec360287866e76237dac2a
Fernando Nogueira <fernandopiresnogueira@gmail.com>
2023-03-17 13:57:17 -0300

    header e lista de categorias adicionados

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header />
   );
+}
+
+function Header(){
+  const tituloApp = "Hoje Aprendi!"
+
+  return(
+    <>
+    <header className="header">
+      <div className="logo">
+        <img src="./logo.jpeg" height="68" width="68" alt="Logo Hoje Aprendi" />
+        <h1>{tituloApp}</h1>
+      </div>
+      <button className="btn">Compartilhe uma curiosidade</button>
+    </header>
+    <main className="main">
+      <ListaCategorias />
+    </main>
+    </>
+  )
+}
+
+const CATEGORIAS = [
+  {nome: 'tecnologia', cor: '#3b82f6'},
+  {nome: 'ciência', cor: '#16a34a'},
+  {nome: 'finanças', cor: '#ef4444'},
+  {nome: 'sociedade', cor: '#eab308'},
+  {nome: 'entretenimento', cor: '#db2777'},
+  {nome: 'saúde', cor: '#14b8a6'},
+  {nome: 'história', cor: '#f97316'},
+  {nome: 'notícias', cor: '#8b5cf6'}
+]
+
+function ListaCategorias(){
+  return(
+    <aside>
+      <ul>
+      {CATEGORIAS.map((cat) => (
+        <li>
+          {cat.nome}
+        </li>
+      ))}
+      </ul>
+    </aside>
+  )
 }
 
 export default App;
